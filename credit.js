88b5721972cb7b70a016d29d9574f4b670407f23
MaterArc <105017592+MaterArc@users.noreply.github.com>
2024-10-27 20:43:31 -0400

    Error Handling

--- credit.js
+++ credit.js
@@ -4,22 +4,32 @@ document.addEventListener('DOMContentLoaded', () => {
 
     grids.forEach(grid => {
         const userId = grid.getAttribute('data-user-id');
-        const avatarHash = grid.querySelector('.grid-img').getAttribute('alt');
+        const avatarHash = grid.querySelector('.grid-img').getAttribute('alt'); 
 
         const avatarUrl = `https://cdn.discordapp.com/avatars/${userId}/${avatarHash}.png?size=1024`;
 
         const imgElement = grid.querySelector('.grid-img');
         imgElement.src = avatarUrl;
-        imgElement.alt = avatarHash;
+        imgElement.alt = avatarHash; 
 
         const img = new Image();
         img.crossOrigin = "Anonymous";
         img.src = avatarUrl;
 
         img.onload = () => {
-            const dominantColor = colorThief.getColor(img);
-            const hexColor = `rgb(${dominantColor[0]}, ${dominantColor[1]}, ${dominantColor[2]})`;
-            grid.style.border = `2px solid ${hexColor}`;
+            try {
+                const dominantColor = colorThief.getColor(img);
+                const hexColor = `rgb(${dominantColor[0]}, ${dominantColor[1]}, ${dominantColor[2]})`;
+                grid.style.border = `5px solid ${hexColor}`;
+            } catch (error) {
+                console.error(`Error extracting color for ${avatarUrl}:`, error);
+                grid.style.border = '5px solid gray';
+            }
+        };
+
+        img.onerror = () => {
+            console.error(`Error loading image from ${avatarUrl}`);
+            grid.style.border = '5px solid gray';
         };
     });
 });
