64ac8f2d3572db4729a7fe60647ff92f0c7147b3
MaterArc <105017592+MaterArc@users.noreply.github.com>
2025-03-08 22:22:06 -0500

    fix

--- credit.js
+++ credit.js
@@ -1,25 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
     const grids = document.querySelectorAll('.grid');
 
-    grids.forEach(grid => {
+    grids.forEach((grid, index) => {
         const userId = grid.getAttribute('data-user-id');
-        const discordUrl = `https://crossorigin.me/https://discordlookup.com/user/${userId}`;
+        const discordUrl = `https://cors-proxy.htmldriven.com/?url=https://discordlookup.com/user/${userId}`;
 
-        fetch(discordUrl)
-            .then(response => response.text())
-            .then(html => {
-                const parser = new DOMParser();
-                const doc = parser.parseFromString(html, 'text/html');
-                const avatarImage = doc.querySelector('img[src*="cdn.discordapp.com/avatars/"]');
+        setTimeout(() => {
+            fetch(discordUrl)
+                .then(response => response.text())
+                .then(html => {
+                    const parser = new DOMParser();
+                    const doc = parser.parseFromString(html, 'text/html');
+                    const avatarImage = doc.querySelector('img[src*="cdn.discordapp.com/avatars/"]');
 
-                if (avatarImage) {
-                    const avatarUrl = avatarImage.src;
-                    const imgElement = grid.querySelector('.grid-img');
-                    imgElement.src = avatarUrl;
-                }
-            })
-            .catch(error => {
-                console.error('Error fetching Discord data:', error);
-            });
+                    if (avatarImage) {
+                        const avatarUrl = avatarImage.src;
+                        const imgElement = grid.querySelector('.grid-img');
+                        imgElement.src = avatarUrl;
+                    }
+                });
+        }, index * 500);
     });
 });
