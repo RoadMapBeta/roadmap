6a6f22cfe97f1ba6ab392e22814acbe3a6774f86
MaterArc <105017592+MaterArc@users.noreply.github.com>
2025-03-08 22:14:57 -0500

    Update credit.js

--- credit.js
+++ credit.js
@@ -20,17 +20,22 @@ document.addEventListener('DOMContentLoaded', () => {
                     const colorThief = new ColorThief();
                     const img = new Image();
                     img.crossOrigin = "Anonymous";
-                    img.src = `https://corsproxy.io/?url=${encodeURIComponent(avatarUrl)}`;
+                    img.src = avatarUrl;
 
                     img.onload = () => {
                         const dominantColor = colorThief.getColor(img);
                         const hexColor = `rgb(${dominantColor[0]}, ${dominantColor[1]}, ${dominantColor[2]})`;
                         imgElement.style.border = `2px solid ${hexColor}`;
                     };
+                } else {
+                    const imgElement = grid.querySelector('.grid-img');
+                    imgElement.src = 'fallback-avatar-url.jpg';  
                 }
             })
             .catch(error => {
                 console.error('Error fetching Discord data:', error);
+                const imgElement = grid.querySelector('.grid-img');
+                imgElement.src = 'https://lh5.googleusercontent.com/proxy/6GqkEKacZBl4xmcSgeJZ_EzDbh4LBdv7J5u1A1HdbAXbU8jrYJHTvk6zyHmHxdA53BphWLT3HLFg0_N3gAwkEbMVF1iIEUZzd3Bs_eM3ACXDwMokenhEQHTLTUL3a7BB_f5JH3oKywsYXbu37KrJ';  
             });
     });
 });
